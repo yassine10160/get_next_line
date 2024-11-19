a77b2ea9dcc684c33ee6662b45eb415070f17a65
Yassine fahfouhi <yassinefahfouhi@Yassines-MacBook-Pro.local>
2024-11-19 17:44:17 +0100

    read with join

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yassinefahfouhi <yassinefahfouhi@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 17:55:06 by yafahfou          #+#    #+#             */
-/*   Updated: 2024/11/19 15:52:05 by yassinefahf      ###   ########.fr       */
+/*   Updated: 2024/11/19 17:42:49 by yassinefahf      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,25 @@
 
 char	*read_file(int fd)
 {
-	char	*buff;
+	char	buff[BUFFER_SIZE + 1];
+	char	*str;
 	ssize_t	bytes;
 
-    buff = NULL;
-	if (BUFFER_SIZE <= 0 || fd == -1 || read(fd,0,0) == -1)
+	bytes = BUFFER_SIZE;
+	if (BUFFER_SIZE <= 0 || fd == -1 || read(fd, 0, 0) == -1)
 		return (buff[0] = '\0', NULL);
-	buff = malloc(BUFFER_SIZE + 1);
-	if (!buff)
-		return (NULL);
-	bytes = read(fd, buff, BUFFER_SIZE);
-	if (bytes == -1)
-		return (NULL);
-	// __builtin_printf("%s\n", buff);
-	return(buff);
+	while (bytes != 0)
+	{
+		bytes = read(fd, buff, BUFFER_SIZE);
+		if (bytes == -1)
+			return (NULL);
+		if (bytes == 0)
+			return (str);
+		buff[BUFFER_SIZE] = '\0';
+		str = ft_strjoin(str, buff);
+		__builtin_printf("je suis la\n");
+	}
+	return (str);
 }
 
 char	*get_next_line(int fd)
@@ -35,21 +40,20 @@ char	*get_next_line(int fd)
 	char	*dest;
 
 	dest = read_file(fd);
-	dest = ft_strchr(dest, '\n', BUFFER_SIZE);
 	return (dest);
 }
 
-// #include <stdio.h>
-// #include <fcntl.h>
+#include <stdio.h>
+#include <fcntl.h>
 
-// int main(int ac, char **av)
-// {
-//     char    *dest;
-//     int     fd;
+int main(int ac, char **av)
+{
+    char    *dest;
+    int     fd;
 
-//     (void)ac;
-//     fd = open(av[1], O_RDONLY);
-//     dest = get_next_line(fd);
-//     printf("%s", dest);
-//     return (0);
-// }
+    (void)ac;
+    fd = open(av[1], O_RDONLY);
+    dest = get_next_line(fd);
+    printf("%s", dest);
+    return (0);
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: yassinefahfouhi <yassinefahfouhi@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 14:40:42 by yassinefahf       #+#    #+#             */
-/*   Updated: 2024/11/19 15:42:04 by yassinefahf      ###   ########.fr       */
+/*   Updated: 2024/11/19 17:36:45 by yassinefahf      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,20 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!s1 || !s2)
 		return (NULL);
 	len1 = ft_strlen(s1);
+    __builtin_printf("%zu\n", len1);
 	len2 = ft_strlen(s2);
 	i = 0;
 	dest = malloc((len1 + len2 + 1) * sizeof(char));
 	if (!dest)
 		return (NULL);
-	while (s1[i])
+	while (s1 && s1[i])
 	{
 		dest[i] = s1[i];
 		i++;
 	}
 	j = 0;
-	while (s2[j])
+    // __builtin_printf("%c\n", dest[0]);
+	while (s2 && s2[j])
 		dest[i++] = s2[j++];
 	dest[i] = '\0';
 	return (dest);
